deb743b783b4cd87d4114a73412918c890f99b47
fuki-saitou <f.saitou@fourm.co.jp>
2024-06-17 15:03:30 +0900

    yeah!!!!

--- src/main/java/fizzbuzz/Step1.java
+++ src/main/java/fizzbuzz/Step1.java
@@ -6,13 +6,16 @@ public class Step1 {
 		
 		for(int i=1;i<=30;i++) {
 			if(i%3==0) {
-				System.out.println("Fizz");
+				if(i%5==0) {
+					System.out.println("FizzBuzz");
+				}else {
+					System.out.println("Fizz");
+				}
 			}else if(i%5==0) {
 				System.out.println("Buzz");
 			}else {
 				System.out.println(i);
 			}
-			
 		}
 	}
 }
